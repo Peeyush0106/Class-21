f68cebe3cd4b97cd86e44197cc303ae4dea4029a
Peeyush0106 <peeyush.agarwal0106@gmail.com>
2020-10-18 16:51:25 +0530

    Class 21

--- myLib.js
+++ myLib.js
@@ -37,3 +37,16 @@ function bounceOff(target1, target2) {
         target1.velocityY *= (-1);
     }
 }
+
+function collide(target1, target2) {
+    if (target1.x - target2.x < (target1.width + target2.width) / 2 &&
+        target2.x - target1.x < (target1.width + target2.width) / 2) {
+        target1.setVelocity(0, 0);
+    }
+
+
+    if (target1.y - target2.y < (target1.height + target2.height) / 2 &&
+        target2.y - target1.y < (target1.height + target2.height) / 2) {
+        target1.setVelocity(0, 0);
+    }
+}
